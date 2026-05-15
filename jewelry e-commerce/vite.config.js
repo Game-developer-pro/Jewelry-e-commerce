9ccc05e6019a2066a3e7db40a5f95792aa6cf089
dear17412@gmail.com <dear17412@gmail.com>
2026-05-15 17:43:03 +0100

    feat: configure Vite server to allow requests from Render host

--- jewelry e-commerce/vite.config.js
+++ jewelry e-commerce/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      "https://jewelry-e-commerce-5nma.onrender.com"
+    ]
+  }
 })
